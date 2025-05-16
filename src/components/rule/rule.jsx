2b3bcbef51bf6e1d1a37666c80f3565438846558
Dilara <dilaraozturk028@gmail.com>
2025-05-16 11:45:40 +0200

    modal

--- src/components/rule/rule.jsx
+++ src/components/rule/rule.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import './rule.css'
-export default function Rule(){
-    return(
+import rule from '../../assets/images/image-rules.svg'
+
+export default function Rule() {
+  const [showRules, setShowRules] = useState(false)
+
+  return (
     <>
-        <div>
-            <button id='btn-rule'>RULES</button>
+      <div>
+        <button id="btn-rule" onClick={() => setShowRules(true)}>RULES</button>
+      </div>
+
+      {showRules && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <img src={rule} alt="Rules" />
+            <button className="close-btn" onClick={() => setShowRules(false)}>X</button>
+          </div>
         </div>
-     </>
-    )
+      )}
+    </>
+  )
 }
